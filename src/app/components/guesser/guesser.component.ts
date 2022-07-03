015c69ded2df59f5d3da1a4a46b0a1124e58dc18
Lorenzo Spoleti <LorenzoSpoleti@outlook.com>
2022-07-03 03:37:25 +0200

    sorted countries, flags in options, removed fluff

--- src/app/components/guesser/guesser.component.ts
+++ src/app/components/guesser/guesser.component.ts
@@ -8,11 +8,6 @@ import { HttpClient } from "@angular/common/http";
 import {countries} from './countries'
 import {countriesDict} from './countriesDict'
 
-export interface State {
-  flag: string;
-  name: string;
-  population: string;
-}
 
 export interface Country {
   code: string;
@@ -27,9 +22,7 @@ export interface Country {
   styleUrls: ['./guesser.component.css']
 })
 export class GuesserComponent implements OnInit {
-  stateCtrl = new FormControl('');
   countryCtrl = new FormControl('');
-  filteredStates: Observable<State[]>;
   filteredCountries: Observable<Country[]>;
   solution: Country;
   bearing: number;
@@ -49,41 +42,9 @@ export class GuesserComponent implements OnInit {
   countries: Country[] = countries;
   countriesDict: Object = countriesDict;
 
-
-  states: State[] = [
-    {
-      name: 'Arkansas',
-      population: '2.978M',
-      // https://commons.wikimedia.org/wiki/File:Flag_of_Arkansas.svg
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/9/9d/Flag_of_Arkansas.svg',
-    },
-    {
-      name: 'California',
-      population: '39.14M',
-      // https://commons.wikimedia.org/wiki/File:Flag_of_California.svg
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/0/01/Flag_of_California.svg',
-    },
-    {
-      name: 'Florida',
-      population: '20.27M',
-      // https://commons.wikimedia.org/wiki/File:Flag_of_Florida.svg
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Florida.svg',
-    },
-    {
-      name: 'Texas',
-      population: '27.47M',
-      // https://commons.wikimedia.org/wiki/File:Flag_of_Texas.svg
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Texas.svg',
-    },
-  ];
-
   constructor(private http: HttpClient) {
     //console.log('called constructor')
 
-    this.filteredStates = this.stateCtrl.valueChanges.pipe(
-      startWith(''),
-      map(state => (state ? this._filterStates(state) : this.states.slice())),
-    );
     this.filteredCountries = this.countryCtrl.valueChanges.pipe(
       startWith(''),
       map(country => (country ? this._filterCountries(country) : this.countries.slice())),
@@ -238,12 +199,6 @@ export class GuesserComponent implements OnInit {
 
   resetGame(){
     this.ngOnInit();
-  }
-
-  private _filterStates(value: string): State[] {
-    const filterValue = value.toLowerCase();
-
-    return this.states.filter(state => state.name.toLowerCase().includes(filterValue));
   }
 
   private _filterCountries(value: string): Country[] {
